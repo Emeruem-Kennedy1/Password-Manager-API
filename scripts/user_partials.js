8c7e53a99035f03b29ff815036ab02553a41bf87
Kennedy Emeruem <kennedyemeruem123@gmail.com>
2022-06-08 12:21:01 -0700

    getUser response includes username and all services

--- scripts/user_partials.js
+++ scripts/user_partials.js
@@ -1,11 +1,13 @@
 const [User, Password] = require('../db/users_database');
 const md5 = require('md5');
+const { response } = require('express');
 
 function getUser(req, res) {
     User.findOne({username: req.params.user}, (err, user) => {
         if (err) {console.log(err);} 
         else if (user) {
-            res.send(user.services);
+            const response = {username: user.username ,services: user.services}
+            res.send(response);
         } else {
             res.send('User not found');
         }
