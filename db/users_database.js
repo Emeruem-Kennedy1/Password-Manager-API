3b729dc996449f7f8087e87da55d05556f7c092a
Kennedy Emeruem <kennedyemeruem123@gmail.com>
2022-06-08 13:10:47 -0700

    changed the user and service schemas

--- db/users_database.js
+++ db/users_database.js
@@ -17,9 +17,6 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    services: {
-        type: Array,
-    }
 });
 
 // * This is the password schema. a password is a combination of the service, username, and password. the password will be stored in the user's passwords array.
@@ -35,6 +32,10 @@ const serviceSchema = new mongoose.Schema({
     username: {
         type: String,
         required: true,
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
     },
 });
 
